91d0e35b617be63b804768bbd38f0c5aa210876a
Pietruszko <gabriel.karalus98@gmail.com>
2025-03-25 16:11:13 +0100

    feat:html template with basic css styling
    - Added home.html, style.css, configured settings.py and views.py

--- djangolearner/djangoapp/views.py
+++ djangolearner/djangoapp/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
-
 def home(request):
-    return HttpResponse('Hello, World!')
+    return render(request, 'home.html')
